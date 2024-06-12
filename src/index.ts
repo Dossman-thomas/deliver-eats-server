3c9980d3b80b2cd0e2c658c4256c1b3c27d50eb0
Dossman-thomas <tdossman.development@gmail.com>
2024-06-12 12:17:54 -0400

    cloudinary set up in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,27 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+
 
 // connect to deliverEats MongoDB
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to MongoDB");
 });
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // set up the express app
 const PORT = parseInt(process.env.PORT as string, 10) || 7000;  // ensure PORT is a number
 const app = express();
 
-// add middleware to parse JSON and urlencoded request bodies
+// add middleware to parse the request body, so we can access req.body in our route handlers
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // add a "health" endpoint to check if the server is running properly. This is useful for monitoring and alerting purposes.
